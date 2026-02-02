5c99ed976c45366a9f818c97e010d8e08ac8376e
s414june <s414june@gmail.com>
2026-02-02 14:29:16 +0800

    fix vite again

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/lottery_web_ui/',
+  base: './',
   build: {
     outDir: 'docs',
   },
